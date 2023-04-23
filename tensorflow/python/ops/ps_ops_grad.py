58fd0dec17bded1bf86f793b821a5236a69313a9
Conley Lee <conleylee@foxmail.com>
2023-04-23 22:27:44 +0800

    support trainable hash table

--- tensorflow/python/ops/ps_ops_grad.py
+++ tensorflow/python/ops/ps_ops_grad.py
@@ -7,6 +7,7 @@ def lookup_embedding_local_ps_grad(op, grad):
     handle = op.inputs[0]
     ids = op.inputs[1]
     if isinstance(grad, ops.IndexedSlices):
+        ids = array_ops.gather(ids, grad.indices)
         grad = grad.values
 
     shape = [ids.shape.as_list()[-1], grad.shape.as_list()[-1]]
